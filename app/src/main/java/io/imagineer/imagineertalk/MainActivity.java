f99a4a4c50ae0fa2884cfb3ca4be4a714478f37a
Minsoo Kwak <marcosdeseul@gmail.com>
2017-06-02 12:41:15 +0800

    Add basic layout

--- app/src/main/java/io/imagineer/imagineertalk/MainActivity.java
+++ app/src/main/java/io/imagineer/imagineertalk/MainActivity.java
@@ -2,12 +2,32 @@ package io.imagineer.imagineertalk;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView mListView;
+    private EditText mMsgText;
+    private Button mSendButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mListView = (ListView) findViewById(R.id.listView);
+        mMsgText = (EditText) findViewById(R.id.msgText);
+        mSendButton = (Button) findViewById(R.id.sendButton);
+
+        String[] samples = {
+                "test1",
+                "test2",
+                "test3",
+        };
+
+        mListView.setAdapter(new ArrayAdapter<String>(this, R.layout.list_item, samples));
     }
 }
