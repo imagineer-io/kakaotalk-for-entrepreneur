1d495681f76bd1f74d0bafad04d708ddb72f252d
Minsoo Kwak <marcosdeseul@gmail.com>
2017-06-02 21:12:19 +0800

    Add signup

--- app/src/main/java/io/imagineer/imagineertalk/AuthActivity.java
+++ app/src/main/java/io/imagineer/imagineertalk/AuthActivity.java
@@ -1,18 +1,28 @@
 package io.imagineer.imagineertalk;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class AuthActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class AuthActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText mEmailEditText;
     private EditText mPasswordEditText;
     private Button mConfirmButton;
 
     private String mPageType;
+    private boolean mIsSignUp;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +33,44 @@ public class AuthActivity extends AppCompatActivity {
         mPasswordEditText = (EditText) findViewById(R.id.passwordEditText);
         mConfirmButton = (Button) findViewById(R.id.confirmButton);
 
+        mAuth = FirebaseAuth.getInstance();
+
         Intent intent = getIntent();
         boolean isSignUp = intent.getBooleanExtra("isSignUp", false);
-        if (isSignUp) {
-            mPageType = "Sign Up";
-        } else {
-            mPageType = "Log In";
-        }
+//        if (isSignUp) {
+//            mPageType = "Sign Up";
+//        } else {
+//            mPageType = "Log In";
+//        }
+        mIsSignUp = isSignUp;
+        mPageType = isSignUp ? "Sign Up" : "Log In";
 
         mConfirmButton.setText(mPageType);
+        mConfirmButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (mEmailEditText.getText().toString() != ""
+                && mPasswordEditText.getText().toString() != "") {
+            // TODO: should implement
+            if (mIsSignUp) {
+
+                mAuth.createUserWithEmailAndPassword(
+                        mEmailEditText.getText().toString(),
+                        mPasswordEditText.getText().toString()
+                ).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            startActivity(new Intent(AuthActivity.this, MainActivity.class));
+                        }
+                    }
+                });
+
+            } else {
+
+            }
+        }
     }
 }
