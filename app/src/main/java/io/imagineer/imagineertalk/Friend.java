c5cd71009f7bf0df4116c89e8a655cc29d010472
Minsoo Kwak <marcosdeseul@gmail.com>
2017-06-02 23:22:00 +0800

    Add friend list

---
+++ app/src/main/java/io/imagineer/imagineertalk/Friend.java
@@ -0,0 +1,24 @@
+package io.imagineer.imagineertalk;
+
+/**
+ * Created by marco on 6/2/17.
+ */
+
+public class Friend {
+    private String email;
+
+    public Friend() {
+    }
+
+    public Friend(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
--- app/src/main/java/io/imagineer/imagineertalk/FriendActivity.java
+++ app/src/main/java/io/imagineer/imagineertalk/FriendActivity.java
@@ -4,14 +4,30 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 public class FriendActivity extends AppCompatActivity {
 
+    private final String CHILD_USERS = "users";
+
+    private DatabaseReference mFirebaseDatabaseReference;
     private FirebaseAuth mAuth;
+
     private Button mLogoutButton;
+    private ListView mFriendListView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +35,14 @@ public class FriendActivity extends AppCompatActivity {
         setContentView(R.layout.activity_friend);
 
         mAuth = FirebaseAuth.getInstance();
-        mLogoutButton = (Button) findViewById(R.id.logoutButton);
+        mFirebaseDatabaseReference = FirebaseDatabase.getInstance().getReference();
 
+        mFriendListView = (ListView) findViewById(R.id.friendListView);
+        ArrayList<String> friends = new ArrayList<>();
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_item, friends);
+        mFriendListView.setAdapter(adapter);
+
+        mLogoutButton = (Button) findViewById(R.id.logoutButton);
         mLogoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -30,5 +52,34 @@ public class FriendActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        ChildEventListener childEventListener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                Friend friend= dataSnapshot.getValue(Friend.class);
+                adapter.add(friend.getEmail());
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        };
+        mFirebaseDatabaseReference.child(CHILD_USERS).addChildEventListener(childEventListener);
     }
 }
