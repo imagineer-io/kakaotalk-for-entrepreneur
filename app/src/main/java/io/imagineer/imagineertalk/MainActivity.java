43a7561c80b5dcca7571d50e081572df024d13cf
Minsoo Kwak <marcosdeseul@gmail.com>
2017-06-02 15:42:02 +0800

    Add firebase data insertion

--- app/src/main/java/io/imagineer/imagineertalk/MainActivity.java
+++ app/src/main/java/io/imagineer/imagineertalk/MainActivity.java
@@ -2,10 +2,14 @@ package io.imagineer.imagineertalk;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,10 +17,14 @@ public class MainActivity extends AppCompatActivity {
     private EditText mMsgText;
     private Button mSendButton;
 
+    private DatabaseReference mFirebaseDatabaseReference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFirebaseDatabaseReference = FirebaseDatabase.getInstance().getReference();
 
         mListView = (ListView) findViewById(R.id.listView);
         mMsgText = (EditText) findViewById(R.id.msgText);
@@ -29,5 +37,14 @@ public class MainActivity extends AppCompatActivity {
         };
 
         mListView.setAdapter(new ArrayAdapter<String>(this, R.layout.list_item, samples));
+
+        mSendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Message message = new Message("Marco", mMsgText.getText().toString());
+                mFirebaseDatabaseReference.child("messages").push().setValue(message);
+                mMsgText.setText("");
+            }
+        });
     }
 }
---
+++ app/src/main/java/io/imagineer/imagineertalk/Message.java
@@ -0,0 +1,40 @@
+package io.imagineer.imagineertalk;
+
+/**
+ * Created by marco on 6/2/17.
+ */
+
+public class Message {
+    private String id;
+    private String sender;
+    private String text;
+
+    public Message(String sender, String text) {
+        this.sender = sender;
+        this.text = text;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
