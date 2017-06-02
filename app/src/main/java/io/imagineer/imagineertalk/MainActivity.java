d5229e4169e7fbfd5f8f42113b8946b978980b9f
Minsoo Kwak <marcosdeseul@gmail.com>
2017-06-02 17:19:25 +0800

    Add data retrieving feature

--- app/src/main/java/io/imagineer/imagineertalk/MainActivity.java
+++ app/src/main/java/io/imagineer/imagineertalk/MainActivity.java
@@ -8,10 +8,17 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private final String CHILD_MESSAGES = "messages";
 
     private ListView mListView;
     private EditText mMsgText;
@@ -30,21 +37,51 @@ public class MainActivity extends AppCompatActivity {
         mMsgText = (EditText) findViewById(R.id.msgText);
         mSendButton = (Button) findViewById(R.id.sendButton);
 
-        String[] samples = {
-                "test1",
-                "test2",
-                "test3",
-        };
-
-        mListView.setAdapter(new ArrayAdapter<String>(this, R.layout.list_item, samples));
+//        String[] samples = {
+//                "test1",
+//                "test2",
+//                "test3",
+//        };
+        ArrayList<String> samples = new ArrayList<>();
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_item, samples);
+        mListView.setAdapter(adapter);
 
         mSendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Message message = new Message("Marco", mMsgText.getText().toString());
-                mFirebaseDatabaseReference.child("messages").push().setValue(message);
+                mFirebaseDatabaseReference.child(CHILD_MESSAGES).push().setValue(message);
                 mMsgText.setText("");
             }
         });
+
+        ChildEventListener childEventListener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                Message message = dataSnapshot.getValue(Message.class);
+                adapter.add(message.getText());
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        };
+        mFirebaseDatabaseReference.child(CHILD_MESSAGES).addChildEventListener(childEventListener);
     }
 }
