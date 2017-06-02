c0e98dd5912d4a3cc4e354c9a12256e7fbf32ad6
Minsoo Kwak <marcosdeseul@gmail.com>
2017-06-02 20:50:08 +0800

    Add intent between AuthActivity and LaunchActivity

--- app/src/main/java/io/imagineer/imagineertalk/AuthActivity.java
+++ app/src/main/java/io/imagineer/imagineertalk/AuthActivity.java
@@ -1,13 +1,36 @@
 package io.imagineer.imagineertalk;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AuthActivity extends AppCompatActivity {
+
+    private EditText mEmailEditText;
+    private EditText mPasswordEditText;
+    private Button mConfirmButton;
+
+    private String mPageType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
+
+        mEmailEditText = (EditText) findViewById(R.id.emailEditText);
+        mPasswordEditText = (EditText) findViewById(R.id.passwordEditText);
+        mConfirmButton = (Button) findViewById(R.id.confirmButton);
+
+        Intent intent = getIntent();
+        boolean isSignUp = intent.getBooleanExtra("isSignUp", false);
+        if (isSignUp) {
+            mPageType = "Sign Up";
+        } else {
+            mPageType = "Log In";
+        }
+
+        mConfirmButton.setText(mPageType);
     }
 }
--- app/src/main/java/io/imagineer/imagineertalk/LaunchActivity.java
+++ app/src/main/java/io/imagineer/imagineertalk/LaunchActivity.java
@@ -1,10 +1,12 @@
 package io.imagineer.imagineertalk;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
-public class LaunchActivity extends AppCompatActivity {
+public class LaunchActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button signUpButton;
     private Button logInButton;
@@ -16,5 +18,31 @@ public class LaunchActivity extends AppCompatActivity {
 
         signUpButton = (Button) findViewById(R.id.signUpButton);
         logInButton = (Button) findViewById(R.id.logInButton);
+
+//        View.OnClickListener listener = new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {}
+//        };
+//        signUpButton.setOnClickListener(listener);
+
+        signUpButton.setOnClickListener(this);
+        logInButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(LaunchActivity.this, AuthActivity.class);
+
+        // if (v.getId() == R.id.signUpButton) {}
+        switch (v.getId()) {
+            case (R.id.signUpButton):
+                intent.putExtra("isSignUp", true);
+                break;
+            case (R.id.logInButton):
+            default:
+                intent.putExtra("isSignUp", false);
+                break;
+        }
+        startActivity(intent);
     }
 }
